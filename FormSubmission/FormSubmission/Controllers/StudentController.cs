aa35921830f19e236949d06f993df129c58cc47c
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-10-29 15:28:14 +0600

    Form and Validation

---
+++ FormSubmission/FormSubmission/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    
+    public class StudentController : Controller
+    {
+        [HttpGet]   
+        public ActionResult Create() {
+            //ViewBag.Name = Request["Name"];
+            //ViewBag.Id = Request["dfdf"];
+            return View(new Student());
+        }
+        [HttpPost]
+        public ActionResult Create(Student s) {
+            if (ModelState.IsValid) {
+                return RedirectToAction("Index","Home");
+            }
+            
+            return View(s);
+        }
+        /*public ActionResult Create(FormCollection fc)
+        {
+            ViewBag.Name = fc["Name"];
+            ViewBag.Id = fc["Id"];
+            return View();
+        }*/
+        /*public ActionResult Create(string Name, string Id) { 
+            
+        }*/
+
+    }
+}
---
+++ FormSubmission/FormSubmission/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage ="Please provide Id")]
+        [Range(1,40,ErrorMessage ="Id must be 1<=id<=40")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string Phone { get; set; }
+    }
+}
